1715ab688e1d527b584fa37d639a1646fdf17350
SRM <nnguy22@u.rochester.edu>
2021-10-24 01:58:56 -0400

    take 2

--- app/src/main/java/com/srm325/mazegame/Menu.java
+++ app/src/main/java/com/srm325/mazegame/Menu.java
@@ -2,30 +2,45 @@ package com.srm325.mazegame;
 
 import com.srm325.mazegame.R;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class Menu extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
+
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
+
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
@@ -34,28 +49,73 @@ public class Menu extends Activity implements OnClickListener {
         Button exit = (Button)findViewById(R.id.bExit);
         newGame.setOnClickListener(this);
         exit.setOnClickListener(this);
-        
-    }
+        makeyourown.setOnClickListener(this);
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
 
-    public static final int PICK_IMAGE = 1;
+    }
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(Menu.this, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(Menu.this, new String[] { permission }, requestCode);
+        }
+        else {
+        } }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
     {
-        if (requestCode == PICK_IMAGE) {
-            //TODO: action
+        super.onRequestPermissionsResult(requestCode,
+                permissions,
+                grantResults);
+
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(Menu.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(Menu.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
+    public static final int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("img", "result");
+        if (resultCode == Activity.RESULT_OK)
+        {
+            if (requestCode == RESULT_LOAD_IMAGE)
+            {
+                Uri selectedImageUri = data.getData();
+                String selectedImagePath = getPath(selectedImageUri);
+
+            }
+        }
+
+
+    }
+
+    Bitmap bitmap=null;
     @Override
     public void onClick(View view) {
         //check which button was clicked with its id
         switch(view.getId()) {
             case R.id.bOwn:
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(galleryIntent, "Select an image"), 0);
             case R.id.bExit:
                 finish();
                 break;
